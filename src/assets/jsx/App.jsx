4cc3b2bfc14d0277b2dbba2d54d4534d16ca22a3
Ollie <olliebyrne2@outlook.com>
2023-08-29 14:52:26 +0100

    styled css and added like button

--- src/assets/jsx/App.jsx
+++ src/assets/jsx/App.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 const artistData = {
   genre: "The King of Soulful Rock",
-  introduction: "Irish singer, songwriter, and multi-instrumentalist Hozier rose to global attention in 2013 when his moody and soulful rock single 'Take Me to Church' became a viral success. The song catapulted him into a major-label contract for his eponymous debut, platinum certifications in 11 countries, and a Grammy nomination for Song of the Year in 2015. After several years of touring and promotion, he returned with the 2018 EP Nina Cried Power and his second full-length, Wasteland, Baby!, which hit number one in the U.S. and Ireland upon its release in 2019. Hozier's first original track of the next decade was the poignant 2022 protest song 'Swan Upon Leda,' which appeared on 2023 LP Unreal Unearth.",
+  introduction: "Irish singer, songwriter, and multi-instrumentalist Hozier rose to global attention in 2013 when his moody and soulful rock single 'Take Me to Church' became a viral success. The song catapulted him into a major-label contract for his eponymous debut, platinum certifications in 11 countries, and a Grammy nomination for Song of the Year in 2015. After several years of touring and promotion, he returned with the 2018 EP Nina Cried Power and his second full-length, Wasteland, Baby!, which hit number one in the U.S. and Ireland upon its release in 2019. Hozier's first original track of the next decade was the poignant 2022 protest song 'Swan Upon Leda,' which appeared on 2023 LP Unreal Unearth. The Dante's Inferno inspired 'Unreal Unearth' album debuted on the 18th of August 2023, to critical acclaim.",
   image: "https://i.scdn.co/image/ab6761610000e5ebad85a585103dfc2f3439119a"
 };
 
@@ -38,23 +38,38 @@ const ArtistInfo = () => (
   </div>
 );
 
-const AlbumList = () => (
-  <div class = "Albums">
-    <h2>Albums</h2>
-    <ol>
-      {albumsData.map(album => (
-        <li key={album.name}>
-          <h3>{album.name}</h3>
-          <p>{album.summary}</p>
-          <p>Release Date: {album.releaseDate}</p>
-          <div class = "image_container">
-          <img class = "image" src={album.coverArt} alt={album.name} />
-          </div>
-        </li>
-      ))}
-    </ol>
-  </div>
-);
+const AlbumList = () => {
+  const [likedAlbums, setLikedAlbums] = useState({});
+
+  const toggleLike = albumName => {
+    setLikedAlbums(prevState => ({
+      ...prevState,
+      [albumName]: !prevState[albumName]
+    }));
+  };
+
+  return (
+    <div class="Albums">
+      <ol>
+        {albumsData.map(album => (
+          <li key={album.name}>
+            <h3>{album.name}</h3>
+            <p>{album.summary}</p>
+            <p>Release Date: {album.releaseDate}</p>
+            <div class="image_container">
+              <img class="image" src={album.coverArt} alt={album.name} />
+            </div>
+            <div class = "like_container">
+            <button class="like_button" onClick={() => toggleLike(album.name)}>
+              {likedAlbums[album.name] ? 'Unlike' : 'Like'}
+            </button>
+            </div>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
 
 
 const NavBar = () => (
